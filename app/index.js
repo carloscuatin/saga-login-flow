26d0a56e4c73e3df67c6ab10344852991424206c
Juan Soto <juan@juansoto.me>
2016-04-17 21:38:08 -0400

    Connect redux to app

--- app/index.js
+++ app/index.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import {Router, Route, browserHistory} from 'react-router'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import reducer from './reducers'
 
 import '../css/main.css'
 
@@ -10,17 +13,21 @@ import Login from './components/pages/Login'
 import Register from './components/pages/Register'
 import NotFound from './components/pages/NotFound'
 
+let store = createStore(reducer)
+
 class LoginFlow extends Component {
   render () {
     return (
-      <Router history={browserHistory}>
-        <Route component={App}>
-          <Route path='/' component={Home} />
-          <Route path='/login' component={Login} />
-          <Route path='/register' component={Register} />
-        </Route>
-        <Route path='*' component={NotFound} />
-      </Router>
+      <Provider store={store}>
+        <Router history={browserHistory}>
+          <Route component={App}>
+            <Route path='/' component={Home} />
+            <Route path='/login' component={Login} />
+            <Route path='/register' component={Register} />
+          </Route>
+          <Route path='*' component={NotFound} />
+        </Router>
+      </Provider>
     )
   }
 }
