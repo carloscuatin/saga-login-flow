9e9495ab649a5f3201caf0a150a6ca3db85acf23
Juan Soto <juan@juansoto.me>
2016-04-18 01:55:40 -0400

    Add redux-logger, put 404 page under `App`

--- app/index.js
+++ app/index.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import {Router, Route, browserHistory} from 'react-router'
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
+import createLogger from 'redux-logger'
 import reducer from './reducers'
 
 import '../css/main.css'
@@ -13,7 +14,10 @@ import Login from './components/pages/Login'
 import Register from './components/pages/Register'
 import NotFound from './components/pages/NotFound'
 
-let store = createStore(reducer)
+let logger = createLogger()
+
+let createStoreWithMiddleware = applyMiddleware(logger)(createStore);
+let store = createStoreWithMiddleware(reducer)
 
 class LoginFlow extends Component {
   render () {
@@ -24,8 +28,8 @@ class LoginFlow extends Component {
             <Route path='/' component={Home} />
             <Route path='/login' component={Login} />
             <Route path='/register' component={Register} />
+            <Route path='*' component={NotFound} />
           </Route>
-          <Route path='*' component={NotFound} />
         </Router>
       </Provider>
     )
