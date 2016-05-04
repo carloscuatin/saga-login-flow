2734c5afd6f43864892b9aa10a6477903e486253
Juan Soto <juan@juansoto.me>
2016-05-04 17:30:00 -0400

    Make dashboard component a stateless function

--- app/components/Dashboard.js
+++ app/components/Dashboard.js
@@ -1,25 +1,16 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
+import React from 'react'
 
-class Dashboard extends Component {
-  render () {
-    return (
-      <article>
-        <section className='text-section'>
-          <h1>Dashboard</h1>
-          <p>
-            Welcome, you are logged in! To have a look at the code behind this application, go to <a href='https://github.com/sotojuan/saga-login-flow'>Github</a>.
-          </p>
-        </section>
-      </article>
-    )
-  }
+function Dashboard () {
+  return (
+    <article>
+      <section className='text-section'>
+        <h1>Dashboard</h1>
+        <p>
+          Welcome, you are logged in! To have a look at the code behind this application, go to <a href='https://github.com/sotojuan/saga-login-flow'>Github</a>.
+        </p>
+      </section>
+    </article>
+  )
 }
 
-function select (state) {
-  return {
-    data: state
-  }
-}
-
-export default connect(select)(Dashboard)
+export default Dashboard
