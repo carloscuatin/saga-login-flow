8c370c2dc6623d9347287171ce2fc3921a8f44d0
Juan Soto <juan@juansoto.me>
2016-05-09 22:33:55 -0400

    Add content to home page

--- app/components/Home.js
+++ app/components/Home.js
@@ -9,7 +9,10 @@ class Home extends Component {
           <section className='text-section'>
             <h1>Welcome to Login Flow!</h1>
             <p>This application demonstrates what a React-based register/login workflow might look like with <a href='https://github.com/yelouafi/redux-saga'>redux-saga</a>.</p>
+
+            <p>It's based on Max Stoiber's <a href='https://github.com/mxstbr/login-flow'>login-flow</a>. See the full source code on <a href='https://github.com/sotojuan/saga-login-flow'>GitHub</a>!</p>
           </section>
+
           <section className='text-section'>
             <h2>Features</h2>
             <ul>
@@ -17,6 +20,11 @@ class Home extends Component {
                 <p>Features will go here :-)</p>
               </li>
             </ul>
+          </section>
+
+          <section className='text-section'>
+            <h2>Authentication</h2>
+            <p>Authentication happens in <code>app/auth/index.js</code>, using <code>fakeRequest.js</code> and <code>fakeServer.js</code>. <code>fakeRequest</code> is a fake <code>XMLHttpRequest</code> wrapper. <code>fakeServer</code> responds to the fake HTTP requests and pretends to be a real server, storing the current users in local storage with the passwords encrypted using <code>bcrypt</code>.</p>
           </section>
         </div>
       </article>
