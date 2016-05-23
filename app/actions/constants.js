f19ecca7710b71cd5a281d98ff5ac7d30a33c3ec
Juan Soto <juan@juansoto.me>
2016-05-23 16:48:31 -0400

    Document actions

--- app/actions/constants.js
+++ app/actions/constants.js
@@ -1,3 +1,8 @@
+/*
+ * These are the variables that determine what our central data store (`../reducers/index.js`)
+ * changes in our state.
+ */
+
 export const CHANGE_FORM = 'CHANGE_FORM'
 export const SET_AUTH = 'SET_AUTH'
 export const SENDING_REQUEST = 'SENDING_REQUEST'
