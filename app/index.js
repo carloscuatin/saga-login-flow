cb9ce0add28fb5f262dd7e82a7dd452e885e5e79
Juan Soto <juan@juansoto.me>
2016-05-23 17:03:22 -0400

    Document entry point

--- app/index.js
+++ app/index.js
@@ -21,18 +21,29 @@ import Dashboard from './components/Dashboard'
 import NotFound from './components/NotFound'
 
 let logger = createLogger({
+  // Ignore `CHANGE_FORM` actions in the logger, since they fire after every keystroke
   predicate: (getState, action) => action.type !== 'CHANGE_FORM'
 })
 
 let sagaMiddleware = createSagaMiddleware()
+
+// Creates the Redux store using our reducer and the logger and saga middlewares
 let store = createStore(reducer, applyMiddleware(logger, sagaMiddleware))
+// We run the root saga automatically
 sagaMiddleware.run(rootSaga)
 
+/**
+* Checks authentication status on route change
+* @param  {object}   nextState The state we want to change into when we change routes
+* @param  {function} replace Function provided by React Router to replace the location
+*/
 function checkAuth (nextState, replace) {
   let {loggedIn} = store.getState()
 
   store.dispatch(clearError())
 
+  // Check if the path isn't dashboard. That way we can apply specific logic to
+  // display/render the path we want to
   if (nextState.location.pathname !== '/dashboard') {
     if (loggedIn) {
       if (nextState.location.state && nextState.location.pathname) {
@@ -42,6 +53,7 @@ function checkAuth (nextState, replace) {
       }
     }
   } else {
+    // If the user is already logged in, forward them to the homepage
     if (!loggedIn) {
       if (nextState.location.state && nextState.location.pathname) {
         replace(nextState.location.pathname)
@@ -52,6 +64,8 @@ function checkAuth (nextState, replace) {
   }
 }
 
+// Mostly boilerplate, except for the routes. These are the pages you can go to,
+// which are all wrapped in the App component, which contains the navigation etc
 class LoginFlow extends Component {
   render () {
     return (
